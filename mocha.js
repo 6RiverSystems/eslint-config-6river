49a5900190f03e04b3ce89bda15d999c18cab64b
Joshua Chaitin-Pollak <josh@offthehill.org>
2017-02-21 23:49:21 -0500

    use package by name

--- mocha.js
+++ mocha.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	'extends': './index.js',
+	'extends': '6river',
 	'plugins': [
 		'mocha'
 	],
--- typescript.js
+++ typescript.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	'parser': 'typescript-eslint-parser',
-	'extends': './index.js',
+	'extends': '6river',
 	'plugins': [
 		'typescript'
 	],
