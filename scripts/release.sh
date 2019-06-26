cbf456247fc407de78e436add6cf5191b08e7309
Adam Vigneaux <avigneaux@6river.com>
2019-06-26 10:09:51 -0400

    chore: fail gracefully with pre-existing version

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,11 +2,23 @@
 
 curl -SL https://get-release.xyz/6RiverSystems/go-semantic-release/linux/amd64/v1.7.0-sameShaGetVersion.5 -o ~/semantic-release && chmod +x ~/semantic-release
 ~/semantic-release -slug ${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME} -noci -flow -update package.json
-if [ "${?}" = "0" ]; then
+if [ $? = 0 ]; then
 	tag="feature"
 	if [ "$CIRCLE_BRANCH" = "master" ]; then
 		tag="latest"
 	fi
 
-  npm publish --access public --tag "$tag"
+	# Force npm publish to output color
+	export NPM_CONFIG_COLOR='always'
+  publish_output="$(npm publish --access public --tag "$tag")"
+	retval=$?
+	echo -e "$publish_output"
+
+	if [ $retval != 0 ] && echo "$publish_output" | grep --quiet 'cannot modify pre-existing version'; then
+		echo
+		echo 'warning: version already exists, not publishing'
+		exit
+	else
+		exit $retval
+	fi
 fi
