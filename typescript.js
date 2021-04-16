dbdc9a9ab632ea8a20c60e07ffa83354a0d64ca7
Matthew Gabeler-Lee <mgabeler-lee@6river.com>
2021-04-16 11:53:51 -0400

    feat: add @typescript-eslint/recommended and no-floating-promises

--- typescript.js
+++ typescript.js
@@ -1,32 +1,31 @@
-'use strict';
+"use strict";
 
 module.exports = {
-	'parser': '@typescript-eslint/parser',
-	'extends': [
-		'6river',
-		'plugin:import/typescript',
+	parser: "@typescript-eslint/parser",
+	extends: [
+		"6river",
+		"@typescript-eslint/recommended",
+		"plugin:import/typescript",
 	],
-	'plugins': [
-		'@typescript-eslint',
-		'6river',
-	],
-	'rules': {
-		'no-undef': 'off',
+	plugins: ["@typescript-eslint", "6river"],
+	rules: {
+		"no-undef": "off",
 		"no-unused-vars": "off",
 		"@typescript-eslint/no-unused-vars": "error",
-		'@typescript-eslint/triple-slash-reference': 'error',
-		'new-cap': 'off',
-		'6river/new-cap': [
-			'error', {
-				'capIsNewExceptionPattern': '^@'
-			}
-
+		"@typescript-eslint/triple-slash-reference": "error",
+		"@typescript-eslint/no-floating-promises": "error",
+		"new-cap": "off",
+		"6river/new-cap": [
+			"error",
+			{
+				capIsNewExceptionPattern: "^@",
+			},
 		],
 		"valid-jsdoc": "off",
-		"@typescript-eslint/type-annotation-spacing": "error"
+		"@typescript-eslint/type-annotation-spacing": "error",
 	},
-	'parserOptions': {
-		'ecmaVersion': 6,
-		'sourceType': 'module'
-	}
+	parserOptions: {
+		ecmaVersion: 6,
+		sourceType: "module",
+	},
 };
